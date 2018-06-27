13eb7ebbf9d1ecb95b0fb3c2f0ffafc85cc34cf7
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-06-28 02:58:40 +0530

    Creating a react component.

--- src/app.js
+++ src/app.js
@@ -1,114 +1,54 @@
 console.log('App Is Running!');
 const appRoot = document.getElementById('app');
 
-let app = {
-  title: 'ToDo App',
-  subtitle: 'This is a simple todo app',
-  options: []
-};
-
-const onFormSubmit = (e) => {
-  e.preventDefault();
-
-  const option = e.target.elements.option.value;
-
-  if (option) {
-    app.options.push(option);
-    e.target.elements.option.value = '';
-    renderTodoApp();
+class Header extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>ToDo App</h1>
+        <h3>Put your life in the hands of a computer</h3>
+      </div>
+    )
   }
-};
+}
 
-const removeAll = () => {
-  app.options = [];
-  renderTodoApp();
-};
+class Action extends React.Component{
+  render(){
+    return(
+      <div>
+        <button>What should I do?</button>
+      </div>
+    )
+  }
+}
 
-const onMakeDecision = () => {
-  const randomNum = Math.floor(Math.random() * app.options.length);
-  console.log(app.options[randomNum]);
-};
+class Options extends React.Component{
+  render(){
+    return(
+      <div>
+        <p>Options component here</p>
+      </div>
+    )
+  }
+}
 
-// JSX - JavaScript XML
-const renderTodoApp = () => {
-  const template = (
-    <div>
-      <h1>{app.title}</h1>
-      {app.subtitle && <p>{app.subtitle}</p>}
-      <p>{app.options.length > 0 ? 'Here are the options' : 'No Options'}</p>
-      <button disabled={app.options.length === 0} onClick={onMakeDecision}>What Should I do?</button>
-      <button disabled={app.options.length === 0} onClick={removeAll}>remove all</button>
-      <ol>
-        {app.options.map(x =>
-          <li key={x}>{x}</li>
-        )}
-      </ol>
-      <form onSubmit={onFormSubmit}>
-        <input type="text" name="option"/>
-        <button>Add Option</button>
-      </form>
-    </div>
-  );
+class AddOption extends React.Component{
+  render(){
+    return(
+      <div>
+        <button>AddOption</button>
+      </div>
+    )
+  }
+}
 
-  ReactDOM.render(template, appRoot);
-};
+const jsx = (
+  <div>
+    <Header/>
+    <Action/>
+    <Options/>
+    <AddOption/>
+  </div>
+);
 
-//renderTodoApp();
-
-
-// Counter App
-let count = 0;
-const increaseCount = () => {
-  count++;
-  renderCounterApp();
-};
-const decreaseCount = () => {
-  count > 0 ? count-- : count;
-  renderCounterApp();
-};
-const resetCount = () => {
-  count = 0;
-  renderCounterApp();
-};
-
-
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={increaseCount}>+1</button>
-      <button onClick={decreaseCount}>-1</button>
-      <button onClick={resetCount}>Reset</button>
-    </div>
-  );
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-//renderCounterApp();
-
-
-
-
-
-
-
-// VisibilityToggle App
-let buttonTitle = 'Show Detail';
-let visibility = false;
-const toggle = () => {
-  visibility = !visibility;
-  renderVisibilityToggleApp();
-};
-
-const renderVisibilityToggleApp = () => {
-  const template = (
-    <div>
-      <h1>Visibility Toggle</h1>
-      <button onClick={toggle}>{visibility ? 'Hide Detail': 'Show Detail'}</button>
-      <p>{visibility && 'Hey, There are some details you can see now.'}</p>
-    </div>
-  );
-  ReactDOM.render(template, appRoot);
-};
-
-renderVisibilityToggleApp();
+ReactDOM.render(jsx, appRoot);
