1748fcebbec1612c15334e84d165ab62e51f1231
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-06-29 02:59:35 +0530

    Stateless functional components added.

--- src/app.js
+++ src/app.js
@@ -57,53 +57,52 @@ class TodoApp extends React.Component{
 };
 
 
-class Header extends React.Component{
-  render(){
-    return(
-      <div>
-        <h1>{this.props.title}</h1>
-        <h3>{this.props.subtitle}</h3>
-      </div>
-    )
-  }
-}
+const Header = (props) => {
+  return(
+    <div>
+      <h1>{props.title}</h1>
+      <h3>{props.subtitle}</h3>
+    </div>
+  )
+};
 
 
-class Action extends React.Component{
-  render(){
-    return(
-      <div>
-        <button onClick={this.props.handlePick} disabled={!this.props.hasOptions}>What should I do?</button>
-      </div>
-    )
-  }
-}
+const Action = (props) =>{
+  return(
+    <div>
+      <button
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        What should I do?
+      </button>
+    </div>
+  )
+};
 
 
-class Options extends React.Component{
-  render(){
-    return(
-      <div>
-        <button onClick={this.props.handleDeleteOptions}>Remove All</button>
-        <p>Options component here</p>
-        {this.props.options.map(option =>
-          <Option key={option} option={option}/>
-        )}
-      </div>
-    )
-  }
-}
+const Options = (props) => {
+  return(
+    <div>
+      <button
+        onClick={props.handleDeleteOptions}
+      >Remove All</button>
+      <p>Options component here</p>
+      {props.options.map(option =>
+        <Option key={option} option={option}/>
+      )}
+    </div>
+  )
+};
 
 
-class Option extends React.Component{
-  render(){
-    return(
-      <div>
-        <p>{this.props.option}</p>
-      </div>
-    )
-  }
-}
+const Option = (props) =>{
+  return(
+    <div>
+      <p>{props.option}</p>
+    </div>
+  )
+};
 
 
 class AddOption extends React.Component{
