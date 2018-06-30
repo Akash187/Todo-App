e67b337a8a31b362f4979fdb5e87fba1c695fe4b
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-06-30 10:29:01 +0530

    ES6 class properties using "transform-class-properties" plugin.

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -5,16 +5,39 @@ import Action from './Action';
 import Header from './Header';
 
 export default class TodoApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
+  state = {
+    options: []
+  };
+
+  handleDeleteOptions = () => {
+    this.setState(() => ({
       options: []
-    };
-  }
+    }));
+  };
+
+  handleDeleteOption = (optionToRemove) => {
+    this.setState((prevState) =>
+      ({
+        options: prevState.options.filter(option => option !== optionToRemove)
+      })
+    )
+  };
+
+  handlePick = () => {
+    const randomNum = Math.floor(Math.random() * this.state.options.length);
+    alert(this.state.options[randomNum]);
+  };
+
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    }
+    this.setState((prevState) =>
+      ({options: prevState.options.concat(option)})
+    );
+  };
 
   componentDidMount(){
     try{
@@ -34,36 +57,6 @@ export default class TodoApp extends React.Component {
       const json = JSON.stringify(this.state.options);
       localStorage.setItem('options', json);
     }
-  }
-
-  handleDeleteOptions() {
-    this.setState(() => ({
-      options: []
-    }));
-  }
-
-  handleDeleteOption(optionToRemove) {
-    this.setState((prevState) =>
-      ({
-        options: prevState.options.filter(option => option !== optionToRemove)
-      })
-    )
-  }
-
-  handlePick() {
-    const randomNum = Math.floor(Math.random() * this.state.options.length);
-    alert(this.state.options[randomNum]);
-  }
-
-  handleAddOption(option) {
-    if (!option) {
-      return 'Enter valid value to add item';
-    } else if (this.state.options.indexOf(option) > -1) {
-      return 'This option already exists';
-    }
-    this.setState((prevState) =>
-      ({options: prevState.options.concat(option)})
-    );
   }
 
   render() {
