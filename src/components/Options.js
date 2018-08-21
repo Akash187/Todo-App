ae5a0e80882785ab48fe09d60febfad709a029ba
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-08-22 00:36:58 +0530

    Refactoring Other Stateless Functional Components.

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Option from './Option';
 
-const Options = (props) => {
-  return (
+const Options = (props) =>(
     <div>
       <button
         onClick={props.handleDeleteOptions}
@@ -15,7 +14,6 @@ const Options = (props) => {
           key={option} option={option}/>
       )}
     </div>
-  )
-};
+  );
 
 export default Options;
